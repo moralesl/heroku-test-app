4d954ff7f5d2741ac7313962611e486ffadb455e
Luis Morales <moralesl@users.noreply.github.com>
2023-08-04 16:06:48 +0200

    Adapt message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Flask!'
+    return 'Hello Flask, from Heroku!'
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
